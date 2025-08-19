6410146619810135441e83e86c004d49a9fc7d77
LalatenduR <143542838+LalatenduR@users.noreply.github.com>
2025-08-20 04:09:30 +0530

    bit more changes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import 'zone.js';
 
 bootstrapApplication(App,{
   providers:[
